1f4b37e7adc0f512926eeab606eca56970a6b5d1
jeonghun <jeonghun@amazon.com>
2023-04-01 15:48:14 +0000

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   app.py
    	modified:   cmd

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import sys
 
 db_path = sys.argv[1]
 app = Flask(__name__)
-db = pymysql.connect(host=db_path, port=3306, user='writeuser', passwd='wjdgns011!', db='dbdata', charset='utf8')
+db = pymysql.connect(host=db_path, port=3306, user='writeuser', passwd='Writeuser1!', db='dbdata', charset='utf8')
 
 @app.route("/")
 def hello_world():
@@ -16,11 +16,26 @@ def hello_world():
 def ping():
     return "ok"
 
+@app.route("/api/getInfo")
+def getInfo() :
+
+    cursor = db.cursor()
+    sql = "select count(*) as cont from joindata"
+    cursor.execute(sql)
+    num = cursor.fetchall()[0][0]
+    cursor.close()
+
+    return {
+        "userName" : "콩순이",
+        "userBank" : "(주)콩순이",
+        "user" : str(num)
+    }
+
 @app.route("/api/getUSDKRW")
 def getUSDKRW() :
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
     url = 'https://quotation-api-cdn.dunamu.com/v1/forex/recent?codes=FRX.KRWUSD'
-    exchange =requests.get(url, headers=headers).json()
+    exchange = requests.get(url, headers=headers).json()
 
     cursor = db.cursor()
     sql = "insert into joindata (price,time) values (%s, %s)"
@@ -34,15 +49,11 @@ def getUSDKRW() :
     return { "exchange" : str(exchange[0]['basePrice']) }
 
 
-@app.route("/api/joinUser")
-def getJoinUser() :
-    cursor = db.cursor()
-    sql = "select count(*) as cont from joindata"
-    cursor.execute(sql)
-    num = cursor.fetchall()[0][0]
-    cursor.close()
+@app.route("/api/test")
+def getTest() :
 
-    return {"user" : str(num)}
+    return "Test Message"
+
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
--- cmd
+++ cmd
@@ -1,1 +1,3 @@
 sudo yum install git
+git clone https://github.com/simjh0919/DRWas-AWS.git ./bank-was
+pip3 install -r ./bank-was/requirements.txt 
